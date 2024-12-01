5a01fb410d200e8bfaeec3dde47f5558b2efec86
michaelfeeney <michaelwcfeeney@gmail.com>
2024-12-01 09:20:36 -0500

    Changed NoodleNexis to DroneAlone

--- NoodleNexus/NoodleNexus/cMonster.cpp
+++
@@ -1,6 +0,0 @@
-#include "cMonster.h"
-
-void cMonster::Attack(cMonster* EnemyMonster)
-{
-	this->pSword->Swish(EnemyMonster);
-}
--- NoodleNexus/NoodleNexus/cSword.cpp
+++
@@ -1,6 +0,0 @@
-#include "cSword.h"
-
-void cSword::Swish(cMonster* pEnemyMonster)
-{
-	pEnemyMonster->health *= 0.25f;
-}
